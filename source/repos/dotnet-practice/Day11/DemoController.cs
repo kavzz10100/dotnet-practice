d30489988610b128f10c1d237f76fb4191ad398c
kavya <143101887+kavzz10100@users.noreply.github.com>
2026-03-11 18:58:32 +0530

    Create DemoController.cs

---
+++ source/repos/dotnet-practice/Day11/DemoController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Services;
+
+namespace MyFirstWebApp.Controllers
+{
+    [Route("demo")]
+    public class DemoController : Controller
+    {
+        private readonly ILogService logService;
+
+        public DemoController(ILogService service)
+        {
+            logService = service;
+        }
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            logService.Log("DemoController.Index Executed");
+            return Content("Demo Index Page");
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            logService.Log("DemoController.About Executed");
+            return Content("Demo About Page");
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            logService.Log("DemoController.Contact Executed");
+            return Content("Demo Contact Page");
+        }
+    }
+}
